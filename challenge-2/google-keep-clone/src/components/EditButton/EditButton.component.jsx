d0b7728b8cc581eccd8a6baa112b2f0d5367762b
Alejandro Huerta <alejandro.huerta@wizeline.com>
2021-11-04 18:29:19 -0600

    Adds push to /notes to edit a note

--- challenge-2/google-keep-clone/src/components/EditButton/EditButton.component.jsx
+++ challenge-2/google-keep-clone/src/components/EditButton/EditButton.component.jsx
@@ -1,13 +1,14 @@
 import React, { useContext } from "react";
 import { MdEdit } from "react-icons/md";
 import { IconButton } from "../Common/Common.styles";
-import { useLocation } from "react-router-dom";
+import { useHistory, useLocation } from "react-router-dom";
 import GlobalContext from "../../providers/Global/GlobalContext";
 import { actions } from "../../utils/reducer/actions";
 
 const EditButton = ({ note }) => {
   const { dispatch } = useContext(GlobalContext);
 
+  const history = useHistory();
   const { pathname } = useLocation();
 
   const handleEdit = (e) => {
@@ -16,6 +17,7 @@ const EditButton = ({ note }) => {
     if (pathname === "/archived-notes") {
       dispatch({ type: actions.deleteArchivedNote, payload: note });
       dispatch({ type: actions.addNote, payload: note });
+      history.push("/notes");
     }
   };
 
